e8a7361bec4e13324b56d782771264ecfff3f01e
AlkalineDreams <craig.dwyer@ocsbstudent.ca>
2017-01-20 22:45:33 -0500

    Rename ResetScene.cs to Scripts/ResetScene.cs

---
+++ Scripts/ResetScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResetScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void turnOffButton(string buttonName)
+    {
+        GameObject thisButton = GameObject.Find(buttonName);
+
+        thisButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("button_off");
+
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
